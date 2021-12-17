[FIX] Off-by-one error while setting up ticks
Also remove unused local variable `n_ticks`, reported by LGTM.com.

Fixes 1eb8822 / #2902.

nilearn/plotting/surf_plotting.py
@@ -342,9 +342,8 @@ def _get_ticks_matplotlib(vmin, vmax, cbar_tick_format):
     n_ticks = 5
     # ...unless we are dealing with integers with a small range
     # in this case, we reduce the number of ticks
-    if cbar_tick_format == "%i" and vmax - vmin < n_ticks:
+    if cbar_tick_format == "%i" and vmax - vmin < n_ticks - 1:
         ticks = np.arange(vmin, vmax + 1)
-        n_ticks = len(ticks)
     else:
         ticks = np.linspace(vmin, vmax, n_ticks)
     return ticks